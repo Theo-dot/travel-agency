3542f997837f05b43104feb1e155e3eb913ae832
Łukasz Spalczyński <spali89@gmail.com>
2019-09-12 10:02:18 +0200

    fixed problem with subsite display and trip filters

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Home from './components/views/Home/Home';
+import Trips from './components/views/Trips/TripsContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import TripContainer from './components/views/Trip/TripContainer';
+import CountryContainer from './components/views/Country/CountryContainer';
+import Info from './components/views/Info/Info';
+import NotFound from './components/views/NotFound/NotFound';
+import {AnimatedSwitch} from 'react-router-transition';
+import parseTrips from './utils/parseTrips';
+import {setMultipleStates} from './redux/globalRedux';
+import styles from './App.scss';
+
+class App extends React.Component {
+  static propTypes = {
+    trips: PropTypes.array,
+    setStates: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    // parse trips when App is first created
+    parseTrips(this.props.trips, this.props.setStates);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
+      // parse trips again if they changed
+      parseTrips(this.props.trips, this.props.setStates);
+    }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <MainLayout>
+          <AnimatedSwitch
+            location={location}
+            atEnter={{opacity: 0, top: 200}}
+            atLeave={{opacity: 0}}
+            atActive={{opacity: 1, top: 0}}
+            className={styles.switchWrapper}
+          >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trips" component={Trips} />
+            <Route path="/trip/:id" component={TripContainer} />
+            <Route exact path="/regions" component={Regions} />
+            <Route exact path="/countries" component={Countries} />
+            <Route path="/country/:countryCode" component={CountryContainer} />
+            <Route exact path="/info" component={Info} />
+            <Route path="*" component={NotFound} />
+          </AnimatedSwitch>
+        </MainLayout>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  trips: state.trips,
+});
+
+const mapDispatchToProps = dispatch => ({
+  setStates: newState => dispatch(setMultipleStates(newState)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, addTag, removeTag, changeDuration} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  changeDuration: (type, value) => dispatch(changeDuration({[type]: value})),
+  addTag: (tag) => dispatch(addTag(tag)),
+  removeTag: (tag) => dispatch(removeTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
