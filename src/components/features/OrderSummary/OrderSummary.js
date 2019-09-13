a85670f69a82fca136c11c394d16c37017a848fb
Łukasz Spalczyński <spali89@gmail.com>
2019-09-13 16:58:51 +0200

    add OrderForm and OrderSummary to show trip price

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import styles from './OrderSummary.scss';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
+
+const OrderSummary = ({price, options}) =>
+  <h2 className={styles.component}>
+    Total $<strong>{calculateTotal(formatPrice(price), options)}</strong>
+  </h2>;
+
+OrderSummary.propTypes = {
+  price: PropTypes.node,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
